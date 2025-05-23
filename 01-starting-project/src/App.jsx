619e836181dfb01d732ce06a13b0e8d5182b0a0f
taeyoung <her020212@naver.com>
2025-05-23 15:53:24 +0900

    feat : useState 훅 추가

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,8 +1,17 @@
 import { CORE_CONCEPTS } from "./data";
 import Header from "./components/Header/Header";
 import Concepts from "./components/Concepts";
+import TabButton from "./components/TabButton";
+import { useState } from "react";
 
 function App() {
+  const [selected, setSelected] = useState("components");
+
+  function handleSelect(name) {
+    setSelected(name);
+    console.log("name", name);
+  }
+
   return (
     <div>
       <Header />
@@ -10,12 +19,27 @@ function App() {
         <h2>Time to get started!</h2>
       </main>
 
-      <h2>concepts</h2>
       <section id="core-concepts">
-        <Concepts {...CORE_CONCEPTS[0]} />
-        <Concepts {...CORE_CONCEPTS[1]} />
-        <Concepts {...CORE_CONCEPTS[2]} />
-        <Concepts {...CORE_CONCEPTS[3]} />
+        <h2>concepts</h2>
+        <ul>
+          {CORE_CONCEPTS.map((list, index) => (
+            <Concepts key={index} {...list} />
+          ))}
+        </ul>
+      </section>
+
+      <section id="examples">
+        <h2>Example</h2>
+        <menu>
+          {/* () => handleSelect 를 사용하면 실행하지 않고 참조만 함 */}
+          {/* 인자가 있을 때 사용해야함 */}
+          <TabButton onSelect={() => handleSelect("components")}>Components</TabButton>
+          <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+          <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+          <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+        </menu>
+
+        {selected == "components" ? <p>component입니다</p> : null}
       </section>
     </div>
   );
--- 01-starting-project/src/components/Concepts.jsx
+++ 01-starting-project/src/components/Concepts.jsx
@@ -3,12 +3,10 @@ import "./Concepts.css";
 export default function Concepts(props) {
   const { image, title, description } = props;
   return (
-    <ul>
-      <li>
-        <img src={image} alt={title} />
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </li>
-    </ul>
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
   );
 }
